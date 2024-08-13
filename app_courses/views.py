2710fbfd9d70deba9da42fd365512b47482e56b6
Anurag Upadhyay <anuragfx818@gmail.com>
2024-08-14 01:13:14 +0530

    added revert in app_opportunities

--- app_courses/views.py
+++ app_courses/views.py
@@ -16,65 +16,59 @@ class cssTemplate(TemplateView):
     template_name = 'app_courses/css.html'
 class HTMLTemplate(TemplateView):
     template_name = 'app_courses/html.html'
-    
 
-class numpyView(View):
-    
-    def get(self, request):
-        details = NumpyMCQModel.objects.all()
-        serializer = NumpyMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/numpy.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/numpy.html', {'data':serializer.data, "length":len(details)})
 
-class PandasView(View):
+# class numpyView(View):
+#     def get(self, request):
+#         details = NumpyMCQModel.objects.all()
+#         serializer = NumpyMCQSerializer(data=details, many = True)
+#         # print(serializer)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/numpy.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/numpy.html', {'data':serializer.data, "length":len(details)})
+
+# class PandasView(View):
+#     def get(self, request):
+#         details = PandasMCQModel.objects.all()
+#         serializer = PandasMCQSerializer(data=details, many = True)
+#         # print(serializer)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/pandas-quiz.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/pandas-quiz.html', {'data':serializer.data, "length":len(details)})
+
     
-    def get(self, request):
-        details = PandasMCQModel.objects.all()
-        serializer = PandasMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/pandas-quiz.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/pandas-quiz.html', {'data':serializer.data, "length":len(details)})
-   
+# class MatplotlibView(View):
+#     def get(self, request):
+#         details = MatplotlibMCQModel.objects.all()
+#         serializer = MatplotlibMCQSerializer(data=details, many = True)
+#         # print(serializer)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/matplotlib-quiz.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/matplotlib-quiz.html', {'data':serializer.data, "length":len(details)})
     
-class MatplotlibView(View):
+# class PythonQuizView(View):
+#     def get(self, request, topictag="basic"):
+#         topictag = topictag.capitalize()
+#         details = PythonMCQModel.objects.filter(topictag = topictag)
+#         serializer = PythonMCQSerializer(data=details, many = True)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/python-quiz.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/python-quiz.html', {'data':serializer.data, "length":len(details)})
     
-    def get(self, request):
-        details = MatplotlibMCQModel.objects.all()
-        serializer = MatplotlibMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/matplotlib-quiz.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/matplotlib-quiz.html', {'data':serializer.data, "length":len(details)})
+# class CSSQuizView(View):
+#     def get(self, request):
+#         details = CSSMCQModel.objects.all()
+#         serializer = CSSMCQSerializer(data=details, many = True)
+#         # print(serializer)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/css-quiz.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/css-quiz.html', {'data':serializer.data, "length":len(details)})
     
-class PythonQuizView(View):
-    
-    def get(self, request):
-        details = PythonMCQModel.objects.all()
-        serializer = PythonMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/python-quiz.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/python-quiz.html', {'data':serializer.data, "length":len(details)})
-    
-class CSSQuizView(View):
-    
-    def get(self, request):
-        details = CSSMCQModel.objects.all()
-        serializer = CSSMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/css-quiz.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/css-quiz.html', {'data':serializer.data, "length":len(details)})
-    
-class LinuxQuizView(View):
-    
-    def get(self, request):
-        details = LinuxMCQModel.objects.all()
-        serializer = LinuxMCQSerializer(data=details, many = True)
-        # print(serializer)
-        if serializer.is_valid():
-            return render(request, 'app_courses/linux-quiz.html', {'data':serializer.data, "length":len(details)})
-        return render(request, 'app_courses/linux-quiz.html', {'data':serializer.data, "length":len(details)})
+# class LinuxQuizView(View):
+#     def get(self, request):
+#         details = LinuxMCQModel.objects.all()
+#         serializer = LinuxMCQSerializer(data=details, many = True)
+#         # print(serializer)
+#         if serializer.is_valid():
+#             return render(request, 'app_courses/linux-quiz.html', {'data':serializer.data, "length":len(details)})
+#         return render(request, 'app_courses/linux-quiz.html', {'data':serializer.data, "length":len(details)})
